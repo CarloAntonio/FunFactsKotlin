470ebd825879b1c59ab439d59ea8df24876edaf7
Carlo Bilbao <cbilbao88@gmail.com>
2017-12-26 18:13:58 -0800

    first commit

---
+++ app/src/main/java/com/riskitbiskit/funfactskotlin/FunFactsActivity.kt
@@ -0,0 +1,12 @@
+package com.riskitbiskit.funfactskotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FunFactsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fun_facts)
+    }
+}

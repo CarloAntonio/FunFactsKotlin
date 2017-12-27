34e2ef7fc27a912a562f9293e052c497325eba02
Carlo Bilbao <cbilbao88@gmail.com>
2017-12-26 22:11:43 -0800

    chore: add color wheel class

--- app/src/main/java/com/riskitbiskit/funfactskotlin/FunFactsActivity.kt
+++ app/src/main/java/com/riskitbiskit/funfactskotlin/FunFactsActivity.kt
@@ -3,17 +3,19 @@ package com.riskitbiskit.funfactskotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RelativeLayout
 import android.widget.TextView
-import java.util.*
 
 class FunFactsActivity : AppCompatActivity() {
 
     //variables
     private val factBook = FactBook()
+    private val colorWheel = ColorWheel()
 
     //views
     private var factTextView: TextView? = null
     private var showFactButton: Button? = null
+    private var relativeLayout: RelativeLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,15 +24,19 @@ class FunFactsActivity : AppCompatActivity() {
         //assign view from the layout file to the corresponding varibales
         factTextView = findViewById(R.id.factTextView)
         showFactButton = findViewById(R.id.showFactButton)
+        relativeLayout = findViewById(R.id.relativeLayout)
 
         //button update the fact TextView with a new fact
         showFactButton!!.setOnClickListener {
 
             //get random fact
             val fact = factBook.getFact()
+            val color = colorWheel.getColor()
 
             //update the screen with new fact
             factTextView!!.text = fact
+            relativeLayout!!.setBackgroundColor(color)
+            showFactButton!!.setTextColor(color)
         }
 
     }

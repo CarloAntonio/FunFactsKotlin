d027b914049d2ad1b071a127062337c4ed20835d
Carlo Bilbao <cbilbao88@gmail.com>
2017-12-26 21:37:49 -0800

    refactor: create a new class that contains and serves facts

--- app/src/main/java/com/riskitbiskit/funfactskotlin/FunFactsActivity.kt
+++ app/src/main/java/com/riskitbiskit/funfactskotlin/FunFactsActivity.kt
@@ -9,6 +9,9 @@ import java.util.*
 class FunFactsActivity : AppCompatActivity() {
 
     //variables
+    private val factBook = FactBook()
+
+    //views
     private var factTextView: TextView? = null
     private var showFactButton: Button? = null
 
@@ -23,23 +26,8 @@ class FunFactsActivity : AppCompatActivity() {
         //button update the fact TextView with a new fact
         showFactButton!!.setOnClickListener {
 
-            //facts
-            val facts = arrayOf(
-                    "Ants stretch when they wake up in the morning.",
-                    "Ostriches can run faster than horses.",
-                    "Olympic gold medals are actually made mostly of silver.",
-                    "You are born with 300 bones; by the time you are an adult you will have 206.",
-                    "It takes about 8 minutes for light from the Sun to reach Earth.",
-                    "Some bamboo plants can grow almost a meter in just one day.",
-                    "The state of Florida is bigger than England.",
-                    "Some penguins can leap 2-3 meters out of the water.",
-                    "On average, it takes 66 days to form a new habit.",
-                    "Mammoths still walked the earth when the Great Pyramid was being built.")
-
-            //randomly select a fact
-            val randomGenerator = Random()
-            val randomNumber = randomGenerator.nextInt(facts.size)
-            val fact = facts[randomNumber]
+            //get random fact
+            val fact = factBook.getFact()
 
             //update the screen with new fact
             factTextView!!.text = fact
